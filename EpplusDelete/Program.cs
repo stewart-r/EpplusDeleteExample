e752ea47826f4291254d6d57f1877796adf4977b
Stewart Robertson <Stewart.Robertson@searchlaboratory.com>
2017-12-21 13:18:04 +0000

    Add project files.

---
+++ EpplusDelete/Program.cs
@@ -0,0 +1,24 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpplusDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myFileInfo = new FileInfo("Demo.xlsx");
+            using (var pack = new ExcelPackage(myFileInfo))
+            {
+                var ws = pack.Workbook.Worksheets.FirstOrDefault();
+                ws.DeleteRow(1, 5, true);
+                pack.SaveAs(new FileInfo("output.xlsx"));
+            }
+        }
+    }
+}
